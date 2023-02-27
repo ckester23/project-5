fdd9df5d210f557f2d9d405ca4528ba63f5d0d19
Cheyanne Kester <ckester@uoregon.edu>
2023-02-26 18:16:46 -0800

    Copied over and modded code from TODOLIST

--- brevets/flask_brevets.py
+++ brevets/flask_brevets.py
@@ -34,6 +34,45 @@ def page_not_found(error):
     app.logger.debug("Page not found")
     return flask.render_template('404.html'), 404
 
+def get_table():
+    """
+    Obtains the newest document in the "lists" collection in database "todo".
+
+    Returns brevet (string) and items (list of dictionaries) as a tuple.
+    """
+    # Get documents (rows) in our collection (table),
+    # Sort by primary key in descending order and limit to 1 document (row)
+    # This will translate into finding the newest inserted document.
+
+    lists = collection.find().sort("_id", -1).limit(1)
+
+    # lists is a PyMongo cursor, which acts like a pointer.
+    # We need to iterate through it, even if we know it has only one entry:
+    for table in lists:
+        # We store all of our lists as documents with two fields:
+        ## brevet: string # brevet value
+        ## items: list   # list of items:
+
+        ### every item has two fields:
+        #### desc: string   # description
+        #### priority: int  # priority
+        return table["brevet"], table["items"]
+
+
+def insert_table(brevet, items):
+    """
+    Inserts a new table into the database "table", under the collection "lists".
+    
+    Inputs a brevet (string) and items (list of dictionaries)
+
+    Returns the unique ID assigned to the document by mongo (primary key.)
+    """
+    output = collection.insert_one({
+        "brevet": brevet,
+        "items": items})
+    _id = output.inserted_id # this is how you obtain the primary key (_id) mongo assigns to your inserted document.
+    return str(_id)
+
 
 ###############
 #
@@ -44,6 +83,59 @@ def page_not_found(error):
 
 # need two more app.route(); one for find, one for insert
 # get info same as getting km
+
+@app.route("/insert_brevet" methods=["POST"])
+def insert_brevet():
+    """
+     control times should be inserted into a MongoDB database, 
+     and the form should be cleared (reset) without refreshing the page.
+    """
+    try:
+        # Read the entire request body as a JSON
+        # This will fail if the request body is NOT a JSON.
+        input_json = request.json
+        # if successful, input_json is automatically parsed into a python dictionary!
+        
+        # Because input_json is a dictionary, we can do this:
+        brevet = input_json["brevet"] # Should be a string
+        rows = input_json["items"]  # Should be a list of dictionaries
+
+        table_id = insert_table(brevet, items)
+
+        return flask.jsonify(result={},
+                        message="Inserted!", 
+                        status=1, # This is defined by you. You just read this value in your javascript.
+                        mongo_id=table_id)
+    except:
+        # The reason for the try and except is to ensure Flask responds with a JSON.
+        # If Flask catches your error, it means you didn't catch it yourself,
+        # And Flask, by default, returns the error in an HTML.
+        # We want /insert to respond with a JSON no matter what!
+        return flask.jsonify(result={},
+                        message="Oh no! Server error!", 
+                        status=0, 
+                        mongo_id='None')
+
+@app.route("/fetch_brevet")
+def fetch_brevet():
+    """
+    /fetch : fetches the newest table from the database.
+
+    Accepts GET requests ONLY!
+
+    JSON interface: gets JSON, responds with JSON
+    """
+    try:
+        brevet, items = get_table()
+        return flask.jsonify(
+                result={"brevet": brevet, "items": items}, 
+                status=1,
+                message="Successfully fetched a table!")
+    except:
+        return flask.jsonify(
+                result={}, 
+                status=0,
+                message="Something went wrong, couldn't fetch any tables!")
 
 
 @app.route("/_calc_times")
